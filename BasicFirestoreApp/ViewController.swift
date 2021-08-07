dd01ba14459164c4f2282c42c96e88da0de37226
Larry Allen <lallen30@yahoo.com>
2021-08-06 22:21:58 -0400

    Initial Commit

---
+++ BasicFirestoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicFirestoreApp
+//
+//  Created by Larry Allen on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
