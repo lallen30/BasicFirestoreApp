4858c4eaacbde5c1242d13f2b1f1d52a9e354f9c
Larry Allen <larry@Larry.local>
2021-08-10 19:08:24 -0400

    firestore test code

--- BasicFirestoreApp/ViewController.swift
+++ BasicFirestoreApp/ViewController.swift
@@ -13,11 +13,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         let db = Firestore.firestore()
         
-        db.collection("teamMembers").getDocuments { (snapshot, error) in
+        
+        guard let path = Bundle.main.path(forResource: "patients", ofType: "json") else {return}
+        //print(path ?? "Path not found")
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            // print(json)
+            
+            guard let array = json as? [Any] else {return}
+            
+            for user in array {
+                guard let userDict = user as? [String: Any] else {return}
+                guard let firstName = userDict["firstName"] as? String else {return}
+                guard let middle = userDict["middle"] as? String else {return}
+                guard let lastName = userDict["lastName"] as? String else {return}
+                guard let DOB = userDict["DOB"] as? String else {return}
+                guard let streetAddress = userDict["streetAddress"] as? String else {return}
+                guard let streetAddress2 = userDict["streetAddress2"] as? String else {return}
+                guard let city = userDict["city"] as? String else {return}
+                guard let state = userDict["state"] as? String else {return}
+                guard let fullName = userDict["fullName"] as? String else {return}
+                guard let status = userDict["status"] as? String else {return}
+                
+                
+                
+                    
+                  //  db.collection("patients").addDocument(data: ["firstName":firstName, "middle":middle, "lastName":lastName, "DOB":DOB, "streetAddress":streetAddress, "streetAddress2":streetAddress2, "city":city, "state":state, "fullName":fullName, "status":status ])
+                
+                print("firstName: \(firstName)")
+                print("middle: \(middle)")
+                print("lastName: \(lastName)")
+                print("DOB: \(DOB)")
+                print("streetAddress: \(streetAddress)")
+                print("streetAddress2: \(streetAddress2)")
+                print("city: \(city)")
+                print("state: \(state)")
+                print("fullName: \(fullName)")
+                print("status: \(status)")
+            
+                    
+                
+                
+                
+            }
+        } catch {
+            print(error)
+        }
+
+        
+        /*
+        db.collection("patients").getDocuments { (snapshot, error) in
             
             if error == nil && snapshot != nil {
                 
@@ -38,8 +88,11 @@ class ViewController: UIViewController {
                 
             }
         }
+        */
         
        // db.collection("teamMembers").addDocument(data: ["name":"Albert Eistein", "DOB":"January 24, 1909", "street1":"123 Some Rd", "stree2":"Suite 456", "city":"Knoxville", "state":"TN", ])
+ 
+ 
     }
 
 
