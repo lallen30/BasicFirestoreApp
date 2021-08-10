ccbdb7770c5c56a8532d5ef91a06937cb2d1a198
Larry Allen <l.allen@gobigwheel.com>
2021-08-10 10:28:24 -0400

    add files

--- BasicFirestoreApp/ViewController.swift
+++ BasicFirestoreApp/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        let db = Firestore.firestore()
+        
+        db.collection("teamMembers").getDocuments { (snapshot, error) in
+            
+            if error == nil && snapshot != nil {
+                
+                for document in snapshot!.documents {
+                    
+                    let documentData = document.data()
+                    print("--------------------------------")
+                    print("--------------------------------")
+                    print("--------------------------------")
+                    
+                    print(documentData)
+                    print("--------------------------------")
+                    print("--------------------------------")
+                    print("--------------------------------")
+                    
+
+                }
+                
+            }
+        }
+        
+       // db.collection("teamMembers").addDocument(data: ["name":"Albert Eistein", "DOB":"January 24, 1909", "street1":"123 Some Rd", "stree2":"Suite 456", "city":"Knoxville", "state":"TN", ])
     }
 
 
---
+++ Pods/Target Support Files/Pods-BasicFirestoreApp/Pods-BasicFirestoreApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BasicFirestoreApp : NSObject
+@end
+@implementation PodsDummy_Pods_BasicFirestoreApp
+@end
